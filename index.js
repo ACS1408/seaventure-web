7d79dc1cafc505e482eee91cfd7b5b00445681fa
acs1408 <abhilash.acs14@gmail.com>
2022-07-04 17:02:03 +0530

    ejs templating and added header and footer as partials

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 
 app.set('view engine', 'ejs')
 
-app.get("/", (req, res) => res.sendFile( __dirname + "/index.html" ));
-app.get("/about", (req, res) => res.sendFile( __dirname + "/about-us.html" ));
+app.get("/", (req, res) => res.render("index"));
+app.get("/about", (req, res) => res.render("about-us"));
 
 app.listen(3000, () => console.log('Server has been started on port 3000'));
