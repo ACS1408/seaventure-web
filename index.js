148463a004277e4407418c427d94ff4b792c06d5
acs1408 <abhilash.acs14@gmail.com>
2022-07-09 15:27:16 +0530

    header section

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const path = require('path');
 const app = express();
-
 app.set('view engine', 'ejs')
+
+app.use(express.static('public'));
 
 app.get("/", (req, res) => res.render("index"));
 app.get("/about", (req, res) => res.render("about-us"));
