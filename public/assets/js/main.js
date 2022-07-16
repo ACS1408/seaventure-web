ab65b05e4babfbf9e5fc9c4234cd22de0b48efba
ACS1408 <abhilash.acs14@gmail.com>
2022-07-16 21:03:09 +0530

    home services desktop

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,3 +2,11 @@ window.addEventListener('scroll', (e) => {
     const scroll = this.scrollY;
     scroll >= 100 ? document.body.classList.add('slim-header') : document.body.classList.remove('slim-header');
 })
+
+const serviceCard = document.querySelectorAll('.service-card');
+serviceCard.forEach((card) => {
+    const cardHeight = card.offsetHeight;
+    const titleHeight = card.querySelector('.service-card__title').offsetHeight;
+    const cardOffset = cardHeight - titleHeight - 33;
+    card.querySelector('.service-card__contents').style.transform = `translateY(${cardOffset}px)`;
+})
